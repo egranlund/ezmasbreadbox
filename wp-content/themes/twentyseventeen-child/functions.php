f96b3f43f74e9b0e2ef51713d108e475c3e5cec7
Eric <granlund.eric@gmail.com>
2017-09-15 04:01:22 -0400

    Dynamic Links
    Added jquery smooth scroll plugin
    Added script for dynamic one page links
    Configured front page links to move smoothly between different areas of home page

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -4,6 +4,16 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+	
+	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+// Enqueue all scripts
+function add_theme_scripts() {
+	
+	// Add dynamic navigation script
+	wp_enqueue_script( 'dynamic_navigation', get_stylesheet_directory_uri().'/assets/js/dynamic_navigation.js', array ( 'jquery' ), 1.0, true);
+}
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
 ?>
