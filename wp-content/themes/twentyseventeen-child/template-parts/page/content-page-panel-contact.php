4a4131372ae5eef997299cb0431c2463b1c66c82
Eric <granlund.eric@gmail.com>
2017-09-17 13:32:01 -0400

    Footer Mods
    Modified the footer to move all of the information to the contact area.

--- wp-content/themes/twentyseventeen-child/template-parts/page/content-page-panel-contact.php
+++ wp-content/themes/twentyseventeen-child/template-parts/page/content-page-panel-contact.php
@@ -28,8 +28,30 @@ global $twentyseventeencounter;
 <div class="panel-content">
 	<div class="wrap">
 		
-		<!-- TODO:  THIS IS EACH PANEL HEADER -->
-		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+		<header class="entry-header">
+			<!-- TODO:  THIS IS EACH PANEL HEADER -->
+			<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+					
+			<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+				<aside class="widget-area" role="complementary">
+				
+					<?php if ( is_active_sidebar( 'sidebar-2' ) ) { ?>
+						<div class="widget-column footer-widget-1">
+							<?php dynamic_sidebar( 'sidebar-2' ); ?>
+						</div>
+					<?php } ?>
+					
+				</aside><!-- .widget-area -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php if ( is_active_sidebar( 'sidebar-3' ) ) { ?>
+				<div class="widget-column footer-widget-2">
+					<?php dynamic_sidebar( 'sidebar-3' ); ?>
+				</div>
+			<?php } ?>
+		</div><!-- .entry-content -->
 		
 	</div><!-- .wrap -->
 </div><!-- .panel-content -->
